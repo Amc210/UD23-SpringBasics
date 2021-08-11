455a3d5c1786b2607450162f4dbfd56c58134052
Amc210 <annamarin513@gmail.com>
2021-08-11 12:38:48 +0200

    Ejercicios Spring

---
+++ ProjectGradle/src/main/java/App/ProjectGradleApplication.java
@@ -0,0 +1,13 @@
+package App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGradleApplication.class, args);
+	}
+
+}
